c85752e3446ce6def431c620740b7ead61b0af0e
Daniel Philippus <dphilippus@mines.edu>
2026-06-23 09:59:59 -0600

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -9,7 +9,6 @@ postfile() {
   	'https://www.hydroshare.org/hsapi/resource/b8852529788a437a8d697e9b0435b99a/files/full_forecasts/' \
 	-H 'accept: application/json' \
 	  -H 'Content-Type: multipart/form-data' \
-	  -H 'X-CSRFTOKEN: ha8ABzzqeqCtHHc3R4XH3UjC39rMW6X5PUnfRyTgx2L6vVghw306qw5EobMU1yVz' \
 	  -F "file=@$1;type=application/json"
 }
 cd full_forecasts
